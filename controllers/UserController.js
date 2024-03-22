8a9983af82ddcef8404f39c3a8b08598eb56195d
Gustavo Eloi <gustavooeloi@gmail.com>
2024-03-22 10:52:49 -0300

    feat: register a new user using bcrypt and jsonwebtoken

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,12 +4,43 @@ import jwt from "jsonwebtoken";
 
 const jwtSecret = process.env.JWT_SECRET;
 
-// Gerenete JWT token
-const generateToken = (user) => {
-  return jwt.sign({ id }, jwtSecret, { expiresIn: "7d" });
+// Generate JWT token
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, jwtSecret, { expiresIn: "7d" });
 };
 
 // Register a new user and sign
 export const register = async (req, res) => {
-  res.send("Register");
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res
+      .status(422)
+      .json({ errors: ["Por favor, utilize um e-mail não utilizado!"] });
+    return;
+  }
+
+  // Generate hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Houve um imprevisto, tente novamente mais tarde"] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
 };
