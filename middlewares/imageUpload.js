d4b65309de345af3bfbf74e326a7793d976327ff
Gustavo Eloi <gustavooeloi@gmail.com>
2024-03-25 09:41:29 -0300

    feat: middleware upload images

---
+++ middlewares/imageUpload.js
@@ -0,0 +1,36 @@
+import multer from "multer";
+import path from "path";
+
+// Where will the image be stored
+const imageStore = multer.diskStorage({
+  destination: (req, file, callback) => {
+    let folder = "";
+
+    if (req.baseUrl.includes("users")) {
+      folder = "user";
+    } else if (req.baseUrl.includes("photos")) {
+      folder = "phot os";
+    }
+
+    callback(null, `uploados/${folder}/`);
+  },
+
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const imageUpload = multer({
+  storage: imageStore,
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg)$/)) {
+      // only png and jpg formats are stored
+      return cb(
+        new Error("Apenas imagens no formato .jpg ou .png são aceitos")
+      );
+    }
+    cb(undefined, true);
+  },
+});
+
+export default imageUpload;
